048a1605646e01e0d6d3471f2884286bb0c55db3
urtinad <urtinadeskaj@gmail.com>
2018-06-17 16:32:02 +0200

    Perfundimi i Web Search Activity

--- app/src/main/java/com/example/pramadani/projekti_v21/SearchActivity.java
+++ app/src/main/java/com/example/pramadani/projekti_v21/SearchActivity.java
@@ -1,18 +1,38 @@
 package com.example.pramadani.projekti_v21;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SearchActivity extends Fragment {
-    @Nullable
+
+    private EditText editTextInput;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View v = inflater.inflate(R.layout.activity_search, container, false);
+
+        editTextInput = v.findViewById(R.id.search_web_edittext);
+
+        v.findViewById(R.id.web_search_button).setOnClickListener(new HandleClick());
+
+        return v;
+    }
+
+    private class HandleClick implements View.OnClickListener {
+        public void onClick(View arg0) {
+            String searchFor = editTextInput.getText().toString();
+            Intent viewSearch = new Intent(Intent.ACTION_WEB_SEARCH);
+            viewSearch.putExtra(SearchManager.QUERY, searchFor);
+            startActivity(viewSearch);
+        }
     }
 }
